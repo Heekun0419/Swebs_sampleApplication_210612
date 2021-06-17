e7ece985e9a2791073ead0ac4c32f5442dc45bb9
LAFI <dnfl4571231@gmail.com>
2021-06-17 21:47:22 +0900

    swebs appbar test

---
+++ app/src/main/java/com/example/swebs_sampleapplication_210612/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.swebs_sampleapplication_210612;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.example.swebs_sampleapplication_210612.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
+
+import org.jetbrains.annotations.NotNull;
+
+public class MainActivity extends FragmentActivity {
+
+    public static final int NUM_PAGES = 3;
+    private boolean isFirst = true;
+    private FragmentStateAdapter adapter;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+       ViewPager2 viewPager =  binding.viewpager2Main;
+        adapter = new ScreenSlidePagerAdapter(this);
+        viewPager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+        viewPager.setAdapter(adapter);
+        viewPager.setCurrentItem(1);
+
+        Toolbar toolbar = findViewById(R.id.toolBar);
+
+        DrawerLayout drawer = binding.drawerLayout;
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.open, R.string.closed);
+        drawer.addDrawerListener(toggle);
+
+
+    }
+
+    private static class ScreenSlidePagerAdapter extends FragmentStateAdapter {
+
+        public ScreenSlidePagerAdapter(FragmentActivity fa) {
+            super(fa);
+        }
+
+        @Override
+        public Fragment createFragment(int position) {
+
+            if (position == 0)
+                return new productionInfoFragment();
+            else if (position == 1)
+                return new ScanFragment();
+            else if (position == 2)
+                return new myPageFragment();
+            else
+                return new ScanFragment();
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return NUM_PAGES;
+        }
+    }
+
+}
+
+
---
+++ app/src/main/java/com/example/swebs_sampleapplication_210612/ScanFragment.java
@@ -0,0 +1,239 @@
+package com.example.swebs_sampleapplication_210612;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.util.Size;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.swebs_sampleapplication_210612.databinding.FragmentScanBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.mlkit.vision.barcode.Barcode;
+import com.google.mlkit.vision.barcode.BarcodeScanner;
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
+import com.google.mlkit.vision.barcode.BarcodeScanning;
+import com.google.mlkit.vision.common.InputImage;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ScanFragment extends Fragment {
+
+    private ListenableFuture cameraProviderFuture;
+    private ExecutorService cameraExecutor;
+    private PreviewView previewView;
+
+    private int CAMERA_PERMISSION_CODE = 4655;
+    private MyImageAnalyser imageAnalyser;
+    private FragmentScanBinding binding;
+
+    public ScanFragment() {
+        // Required empty public constructor
+    }
+
+    public static ScanFragment newInstance(String param1, String param2) {
+        ScanFragment fragment = new ScanFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentScanBinding.inflate(getLayoutInflater());
+
+        previewView = binding.cameraPreView;
+        requireActivity().getWindow().setFlags(1024, 1024);
+
+        imageAnalyser = new MyImageAnalyser(requireActivity().getSupportFragmentManager());
+
+        cameraExecutor = Executors.newSingleThreadExecutor();
+        cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext());
+
+        cameraProviderFuture.addListener(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    if(ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                        ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+                    }
+                    ProcessCameraProvider processCameraProvider = (ProcessCameraProvider) cameraProviderFuture.get();
+                    bindPreView(processCameraProvider);
+                } catch (ExecutionException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, ContextCompat.getMainExecutor(requireContext()));
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        cameraProviderFuture.addListener(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    if(ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                        ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+                    }
+                    ProcessCameraProvider processCameraProvider = (ProcessCameraProvider) cameraProviderFuture.get();
+                    bindPreView(processCameraProvider);
+                } catch (ExecutionException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, ContextCompat.getMainExecutor(requireContext()));
+    }
+
+    private void bindPreView(ProcessCameraProvider processCameraProvider) {
+        Preview preview = new Preview.Builder().build();
+        CameraSelector cameraSelector = new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        ImageCapture imageCapture = new ImageCapture.Builder().build();
+        processCameraProvider.unbindAll();
+        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                .setTargetResolution(new Size(1280,720))
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build();
+        imageAnalysis.setAnalyzer(cameraExecutor,imageAnalyser);
+        processCameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture, imageAnalysis);
+    }
+
+    class MyImageAnalyser implements ImageAnalysis.Analyzer {
+        private FragmentManager fragmentManager;
+        private boolean isUrlFounded = false;
+        MyImageAnalyser(FragmentManager fragmentManager) {
+            this.fragmentManager = fragmentManager;
+
+        }
+
+        @Override
+        public void analyze(@NonNull @NotNull ImageProxy image) {
+            ScanBarcode(image);
+        }
+
+
+        private void ScanBarcode(ImageProxy imageProxy) {
+            @SuppressLint("UnsafeOptInUsageError") Image image1 = imageProxy.getImage();
+
+            assert image1 != null;
+            InputImage inputImage = InputImage.fromMediaImage(image1, imageProxy.getImageInfo().getRotationDegrees());
+            BarcodeScannerOptions options = new BarcodeScannerOptions.Builder()
+                    .setBarcodeFormats(
+                            Barcode.FORMAT_QR_CODE,
+                            Barcode.FORMAT_AZTEC
+                    ).build();
+
+            BarcodeScanner scanner = BarcodeScanning.getClient(options);
+
+            Task<List<Barcode>> result = scanner.process(inputImage)
+                    .addOnSuccessListener(new OnSuccessListener<List<Barcode>>() {
+                        @Override
+                        public void onSuccess(List<Barcode> barcodes) {
+                            // Task completed successfully
+                            // ...
+                            readerBarCodeData(barcodes);
+                            imageProxy.close();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            // Task failed with an exception
+                            // ...
+                        }
+                    })
+                    .addOnCompleteListener(new OnCompleteListener<List<Barcode>>() {
+                        @Override
+                        public void onComplete(@NonNull @NotNull Task<List<Barcode>> task) {
+                        }
+                    });
+        }
+
+        private void readerBarCodeData(List<Barcode> barcodes) {
+            for (Barcode barcode : barcodes) {
+                Rect bounds = barcode.getBoundingBox();
+                Point[] corners = barcode.getCornerPoints();
+                String rawValue = barcode.getRawValue();
+
+                int valueType = barcode.getValueType();
+                // See API reference for complete list of supported types
+                switch (valueType) {
+                    case Barcode.TYPE_WIFI:
+                        String ssid = barcode.getWifi().getSsid();
+                        String password = barcode.getWifi().getPassword();
+                        int type = barcode.getWifi().getEncryptionType();
+                        break;
+                    case Barcode.TYPE_URL:
+                            String Url = barcode.getUrl().getUrl();
+                            String title = barcode.getUrl().getTitle();
+                            ShowMyDialog(Url);
+                            break;
+                }
+            }
+        }
+
+        private void ShowMyDialog(String URl){
+            AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+
+            builder.setTitle("스캔 정보").setMessage(URl);
+            builder.setCancelable(true);
+            builder.setPositiveButton("링크 접속", (dialog, which) -> {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(URl));
+                startActivity(intent);
+            });
+            builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+
+    }
+}
